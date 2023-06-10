58286b50561e96e93b3442c708e8c819c80e47f3
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2023-06-10 10:28:00 +0700

    reinit project

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
